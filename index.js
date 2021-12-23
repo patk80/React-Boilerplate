3940d25eebd0fecdc0780aa49136538d81b3b6aa
Pat Kelly <50188152+patk80@users.noreply.github.com>
2021-12-23 12:02:32 -0500

    Initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/* 
+CLASS COMPONENTS:
+-Accept Props and render content
+-The "traditional" React component
+-Must have a render method
+-Must return something 
+*/
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <p>Embedded JavaScript: (35 * 5) = {35 * 5}</p>
+                <img src="img/think-this-album-cover.jpg" />
+            </div>
+        );
+    }
+}
+
+/*  
+FUNCTION COMPONENTS
+-Accept Props and render content
+-Historically used for simpler components
+-Historically couldn't use important features like State/Lifecycle Methods
+-HOWEVER, with the introduction to Hooks, we can now write full-features function components
+*/
+function Hello2() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h2>Hello World</h2>
+            <h3>Hello World</h3>
+        </div>
+    );
+}
+
+/* What component React DOM should render, where to render the component */
+ReactDOM.render(<Hello />, document.getElementById("root"));
