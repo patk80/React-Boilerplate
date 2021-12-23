457214e18f8711c0e9e7d3c0e4c1899cca250d1f
patk80 <patk80@gmail.com>
2021-12-23 12:29:56 -0500

    Creating sperate file for component

---
+++ Hello.js
@@ -0,0 +1,37 @@
+/* 
+CLASS COMPONENTS:
+-Accept Props and render content
+-The "traditional" React component
+-Must have a render method
+-Must return something 
+*/
+class Hello extends React.Component {
+    render() {
+        return (
+            /* Can only return ONE element. Can put a bunch of things in one <div> */
+            <div>
+                <h1>Hello World</h1>
+                <p>Embedded JavaScript: (35 * 5) = {35 * 5}</p>
+                <img src="img/think-this-album-cover.jpg" />
+            </div>
+        );
+    }
+}
+
+/*  
+FUNCTION COMPONENTS
+-Accept Props and render content
+-Historically used for simpler components
+-Historically couldn't use important features like State/Lifecycle Methods
+-HOWEVER, with the introduction to Hooks, we can now write full-features function components
+*/
+function Hello2() {
+    return (
+        /* Can only return ONE element. Can put a bunch of things in one <div> */
+        <div>
+            <h1>Hello World</h1>
+            <h2>Hello World</h2>
+            <h3>Hello World</h3>
+        </div>
+    );
+}
--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-/* 
-CLASS COMPONENTS:
--Accept Props and render content
--The "traditional" React component
--Must have a render method
--Must return something 
-*/
-class Hello extends React.Component {
+class App extends React.Component {
     render() {
-        return (
-            /* Can only return ONE element. Can put a bunch of things in one <div> */
+        return(
             <div>
-                <h1>Hello World</h1>
-                <p>Embedded JavaScript: (35 * 5) = {35 * 5}</p>
-                <img src="img/think-this-album-cover.jpg" />
+                <Hello />
             </div>
         );
     }
 }
 
-/*  
-FUNCTION COMPONENTS
--Accept Props and render content
--Historically used for simpler components
--Historically couldn't use important features like State/Lifecycle Methods
--HOWEVER, with the introduction to Hooks, we can now write full-features function components
-*/
-function Hello2() {
-    return (
-        /* Can only return ONE element. Can put a bunch of things in one <div> */
-        <div>
-            <h1>Hello World</h1>
-            <h2>Hello World</h2>
-            <h3>Hello World</h3>
-        </div>
-    );
-}
-
 /* What component React DOM should render, where to render the component */
-ReactDOM.render(<Hello />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
